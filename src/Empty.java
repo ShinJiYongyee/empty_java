fccdbfabbb78c4c810ab4a02d7986d9bc5dbaf8e
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-08 21:58:22 +0900

    question8

--- src/Empty.java
+++ src/Empty.java
@@ -1,62 +1,61 @@
 import java.util.Scanner;
-class Day{
-    private String work;
-    public void set(String work){this.work=work;}
-    public String get(){return work;}
-    public void show(){
-        if(work==null) System.out.println("없습니다.");
-        else System.out.println(work+"입니다.");
-    }
+
+class Phone{
+    String name;
+    String tel;
+    void set(String name,String tel){this.name=name;this.tel=tel;}
+
 }
-class MonthSchedule{
+class PhoneBook{
+    int index;
     Scanner input=new Scanner(System.in);
-    int days;
-    Day[] Monthdays;
-    MonthSchedule(int days){
-        this.days =days;
-        this.Monthdays=new Day[days];
-        for(int i=0;i<days;i++){
-            Monthdays[i]=new Day();
-        }
-    }
-    void run(){
-        System.out.println("이번달 스케줄 관리 프로그램.");
-        for(int i = 0; i< days; i++){
-            System.out.print("할일(입력:1, 보기:2, 끝내기:3) >>");
-            int anInt=input.nextInt();
-            if(anInt==1){
-                input();
-            }else if(anInt==2){
-                view();
-            }else if(anInt==3){
-                finish();
-                break;
-            }else{
-                System.out.println("wrong input.");
-            }
+    Phone[] PhoneNumbers;
+    PhoneBook(int index){
+        this.index=index;
+        this.PhoneNumbers=new Phone[index];
+        for(int i=0;i<index;i++){
+            PhoneNumbers[i]=new Phone();
         }
     }
     void input(){
-        System.out.print("날짜(1~"+days+")?");
-        int d=input.nextInt();
-        System.out.print("할일(빈칸없이입력)?");
-        String work=input.next();
-        Monthdays[d].set(work);
+        for(int i=0;i<index;i++){
+            System.out.print("이름과 전화번호(이름과 번호는 빈 칸없이 입력)>>");
+            String name=input.next();
+            String tel=input.next();
+            PhoneNumbers[i].set(name,tel);
+        }
+        System.out.println("저장되었습니다.");
     }
-    void view(){
-        System.out.print("날짜(1~"+days+")?");
-        int d=input.nextInt();
-        System.out.print(d+"일의 할 일은 ");
-        Monthdays[d].show();
-    }
-    void finish(){
-        System.out.println("프로그램을 종료합니다.");
-    }
-}
+    void search(){
+        while (true){
+            System.out.print("검색할 이름>>");
+            String name=input.next();
+            boolean b=false;
+            if(name.equals("그만"))break;
+            for(Phone p:PhoneNumbers){
+                if(p.name.equals(name)){
+                    System.out.println(p.name+"의 번호는"+p.tel+"입니다.");
+                    b=true;
+                }
+            }
+            if(!b)System.out.println(name+" 이 없습니다.");
 
+        }
+    }
+    void run(){
+        input();
+        search();
+        input.close();
+    }
+
+
+}
 public class Empty {
     public static void main(String[] args) {
-        MonthSchedule april=new MonthSchedule(30);
-        april.run();
+        Scanner input=new Scanner(System.in);
+        System.out.print("인원수>>");
+        int n=input.nextInt();
+        var p=new PhoneBook(n);
+        p.run();
     }
 }
