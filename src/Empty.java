10fb4b4b2409b8f35f001f91b2f2d60407974816
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-08 19:44:25 +0900

    empty document

--- src/Empty.java
+++ src/Empty.java
@@ -1,24 +1,7 @@
-class Song{
-    String title;
-    String artist;
-    int year;
-    String country;
-    Song(){}
-    Song(String t,String a,int y,String c){
-        this.title=t;
-        this.artist=a;
-        this.year=y;
-        this.country=c;
-    }
-    void show(){
-        System.out.println(year+"년 "+country+"국적의 "+artist+"가 부른 "+title);
-    }
 
-}
 
 public class Empty {
     public static void main(String[] args) {
-        Song Dancing_Queen=new Song("Dancing Queen","ABBA",1978,"스웨덴");
-        Dancing_Queen.show();
+
     }
 }
