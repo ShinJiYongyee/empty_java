fca1924f2f1ccc8740f9f5ffc26cbb23c838da5f
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-08 18:52:39 +0900

    Changes

--- src/Empty.java
+++ src/Empty.java
@@ -1,31 +1,24 @@
-import java.util.Scanner;
-
-class Grade{
-    int math;
-    int science;
-    int english;
-    Grade(int m,int s,int e){
-        this.math=m;
-        this.science=s;
-        this.english=e;
+class Song{
+    String title;
+    String artist;
+    int year;
+    String country;
+    Song(){}
+    Song(String t,String a,int y,String c){
+        this.title=t;
+        this.artist=a;
+        this.year=y;
+        this.country=c;
     }
-    int average(){
-        return (math+science+english)/3;
+    void show(){
+        System.out.println(year+"년 "+country+"국적의 "+artist+"가 부른 "+title);
     }
 
 }
 
 public class Empty {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-
-        System.out.println("수학,과학,영어 순으로 3개의 점수 입력>>");
-        int math=input.nextInt();
-        int science=input.nextInt();
-        int english=input.nextInt();
-        Grade me=new Grade(math,science,english);
-        System.out.println("평균은 "+me.average());
-
-        input.close();
+        Song Dancing_Queen=new Song("Dancing Queen","ABBA",1978,"스웨덴");
+        Dancing_Queen.show();
     }
 }
