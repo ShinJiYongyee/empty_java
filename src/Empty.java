e072e075963f39066f4ea510eb72a99b2de496d4
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-14 20:47:51 +0900

    question 7

--- src/Empty.java
+++ src/Empty.java
@@ -28,14 +28,38 @@ class ColorPoint extends Point{
         return color+"색의 "+"("+getX()+","+getY()+")의 점";
     }
 }
+class Point3D extends Point{
+    int z;
+    public Point3D(int x, int y,int z) {
+        super(x, y);
+        this.z=z;
+    }
+
+    protected void move(int x, int y,int z) {
+        super.move(x, y);
+        this.z=z;
+    }
+
+    public void moveUp(){
+        this.z+=1;
+    }
+    public void moveDown(){
+        this.z-=1;
+    }
+    public String toString(){
+        return "("+getX()+","+getY()+","+z+")의 점";
+    }
+}
 public class Empty {
     public static void main(String[] args) {
-        ColorPoint zeroPoint=new ColorPoint();
-        System.out.println(zeroPoint.toString()+"입니다.");
-
-        ColorPoint cp=new ColorPoint(10,10);
-        cp.setXY(5,5);
-        cp.setColor("RED");
-        System.out.println(cp.toString()+"입니다");
+        Point3D p=new Point3D(1,2,3);
+        System.out.println(p.toString()+"입니다.");
+        p.moveUp();
+        System.out.println(p.toString()+"입니다.");
+        p.moveDown();
+        p.move(10,10);
+        System.out.println(p.toString()+"입니다.");
+        p.move(100,200, 300);
+        System.out.println(p.toString()+"입니다.");
     }
 }
