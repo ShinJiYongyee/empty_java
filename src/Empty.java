afa585c1d16c2ae282bd5a59998d40fac650cf4d
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-14 19:15:34 +0900

    question 2

--- src/Empty.java
+++ src/Empty.java
@@ -15,9 +15,23 @@ class ColorTV extends TV{
         System.out.println(getSize()+"인치"+" "+color+"컬러");
     }
 }
+
+class IPTV extends ColorTV{
+    String ip;
+    IPTV(String ip,int size,int color){
+        super(size,color);
+        this.ip=ip;
+    }
+
+    @Override
+    void printProperty() {
+        System.out.print("나의 IPTV는 "+ip+" 주소의"+" ");
+        super.printProperty();
+    }
+}
 public class Empty {
     public static void main(String[] args) {
-        ColorTV myTV=new ColorTV(32,1024);
-        myTV.printProperty();
+        IPTV iptv=new IPTV("192.1.1.2",32,2048);
+        iptv.printProperty();
     }
 }
