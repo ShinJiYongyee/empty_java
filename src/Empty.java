2e390555269a1102ff25447e1cd312e7d4306d3c
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-16 23:42:35 +0900

    question 4

--- src/Empty.java
+++ src/Empty.java
@@ -1,10 +1,6 @@
-import java.util.Objects;
 
-import etc.Calc;
 
 public class Empty {
-    public static void main(String[] args) {
-        Calc c=new Calc(10,20);
-        System.out.println(c.sum());
-    }
+
+
 }
---
+++ src/derived/Circle.java
@@ -0,0 +1,12 @@
+package derived;
+
+import base.Shape;
+
+public class Circle extends Shape {
+    public void draw() {
+        System.out.println("Circle");
+    }
+    public void sex(){
+        System.out.println("sex");
+    }
+}
