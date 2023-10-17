a7fcc74fde1dc9c59e7d7e55b44868ddb5eadfb4
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-17 19:30:54 +0900

    question 4

--- src/Empty.java
+++ src/Empty.java
@@ -1,33 +1,24 @@
-import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Empty {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.print("나라 이름과 인구를 입력하세요(에: Korea 5000)");
-        var v=new HashMap<String,Integer>();
-        System.out.println();
+        var v=new Vector<Integer>();
         while (true){
-            System.out.print("나라 이름, 인구 >> ");
-            String s=input.next();
-            if(s.equals("그만"))break;
-            //int를 읽기 전 s.equals를 수행해야 조건문이 정상적으로 작동
-            //int p~가 if문보다 먼저 올 경우 그만+정수 를 읽어야 break
-            int p=input.nextInt();
-            v.put(s,p);
-        }
-
-        while (true){
-            System.out.print("인구 검색 >> ");
-            String s=input.next();
-            if(s.equals("그만"))break;
-            if(!v.containsKey(s)) {
-                System.out.println(s+" 나라는 없습니다");
+            System.out.print("강수량 입력(0 입력시 종료)>> ");
+            int sum=0;
+            int a=input.nextInt();
+            if(a==0)break;
+            v.add(a);
+            for(int b:v){
+                System.out.print(b+" ");
             }
-            else {
-                int p= v.get(s);
-                System.out.println(s+"의 인구는 "+p);
+            System.out.println();
+            for(int b:v){
+                sum+=b;
             }
+            System.out.println("현재 평균 "+sum/v.size());
         }
 
     }
