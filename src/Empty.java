f1795db91b3d9e2ded1e7c178534c10d05d52c47
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-17 18:37:34 +0900

    question 2

--- src/Empty.java
+++ src/Empty.java
@@ -1,24 +1,31 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Empty {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.print("정수(-1이 입력될 때까지)>>");
-        var v=new Vector<Integer>();
-        do{
-            int a=input.nextInt();
-            if(a==-1)break;
+        System.out.print("6개 학점을 빈 칸으로 분리 입력(A/B/C/D/F)>>");
+        var v=new ArrayList<Character>();
+        for(int i=0;i<6;i++){
+            char a=input.next().charAt(0);
             v.add(a);
-        }while (true);
 
-        Iterator<Integer> it=v.iterator();
-        int big=v.get(0);
-        while (it.hasNext()){
-            if(big<it.next())big= it.next();
         }
-        System.out.print("가장 큰 수는 "+big);
+
+        Iterator<Character> it=v.iterator();
+        double sum=0;
+        while (it.hasNext()){
+            char a=it.next();
+            if(a=='A')sum+=4.0;
+            if(a=='B')sum+=3.0;
+            if(a=='C')sum+=2.0;
+            if(a=='D')sum+=1.0;
+            if(a=='F')sum+=0.0;
+
+        }
+        double avg=sum/6;
+        System.out.println(avg);
     }
 
 }
