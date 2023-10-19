10d958caf832bbf6a477c2bfee63aacc1503c7ac
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-19 18:40:48 +0900

    question 6

--- src/Empty.java
+++ src/Empty.java
@@ -1,67 +1,53 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-class Student {
-/*
-    String name;
-*/
-    String major;
-    int id;
-    double avg;
+class Location{
 
-    Student( String major, int id, double avg) {
-        this.major = major;
-        this.id = id;
-        this.avg = avg;
+    int latitude;
+    int longitude;
+    Location(int latitude,int longitude){
+        this.latitude=latitude;
+        this.longitude=longitude;
     }
 
     @Override
     public String toString() {
-        return "학과 : " + major +
-                "\n학번 : " + id + "\n학점평균 : " + avg + "\n";
+        return latitude+"\t"+longitude;
     }
 }
 
 public class Empty {
     public static void main(String[] args) {
-        var std = new HashMap<String,Student>();
-        Scanner input = new Scanner(System.in);
-        System.out.println("학생 이름, 학과, 학번, 학점평균을 입력하세요");
+        Scanner input=new Scanner(System.in);
+        var location=new HashMap<String,Location>();
 
-/*        for (int i = 0; i < 4; i++) {
-            System.out.print( ">> ");
-            String line=input.nextLine();
-            String[] parts=line.split(",");
-            String name = parts[0];
-            String major = parts[1];
-            int id = Integer.parseInt(parts[2]);
-            double avg = Double.parseDouble((parts[3]));
-
-            Student student = new Student(name, major, id, avg);
-            std.add(student);
-        }*/
-
+        System.out.print("도시,경도,위도를 입력하세요");
+        System.out.println();
         for (int i = 0; i < 4; i++) {
-            System.out.print( ">> ");
-
-            String name = input.next();
-            String major = input.next();
-            int id = input.nextInt();
-            double avg = input.nextDouble();
-
-            Student student = new Student(major, id, avg);
-            std.put(name,student);
+            System.out.print(">>");
+            String s= input.nextLine();
+            String[] parts = s.split(",\\s+");
+            String n = parts[0];
+            int latitude = Integer.parseInt(parts[1]);
+            int longitude = Integer.parseInt(parts[2]);
+            Location l = new Location(latitude, longitude);
+            location.put(n, l);
         }
+        for (Map.Entry<String,Location> entry:location.entrySet()) {
+            String n= entry.getKey();
+            Location s=entry.getValue();
+            System.out.println(n+"\t"+s);
 
-        for (HashMap.Entry<String, Student> entry : std.entrySet()) {
-            String key = entry.getKey();
-            Student value = entry.getValue();
-            System.out.println("이름 : " + key + "\n"+value);
         }
+        System.out.print("도시 이름 >> ");
+        String find=input.next();
+        if(location.containsKey(find)){
+            Location n=location.get(find);
+            System.out.println(find+"\t"+n);
+        }
+        else System.out.println(find+"는 없습니다.");
 
-        System.out.print("학생 이름>> ");
-        String name= input.next();
-        System.out.println(std.get(name));
 
     }
 }
