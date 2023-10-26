320d949754af5c5b3f83568bb1d1ffa20f2a8f67
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-26 13:43:09 +0900

    question 10

--- src/Empty.java
+++ src/Empty.java
@@ -1,38 +1,25 @@
-class ArrayUtil{
+import java.util.Scanner;
 
-    public static int [] concat(int[] a, int [] b){
-        int [] c=new int[a.length+ b.length];
-
-        //배열 a와 b를 연결한 새로운 배열 리턴
-/*
-        for (int i = 0; i < a.length; i++) {
-            c[i]=a[i];
+class Dictionary{
+    private static String [] kor={"사랑","아기","돈","미래","희망"};
+    private static String [] eng={"love","baby","money","future","hope"};
+    public static String kor2Eng(String word){
+        for (int i = 0; i < kor.length; i++) {
+            if(kor[i].equals(word))return word+"은/는 "+eng[i];
         }
-        for (int i = a.length; i <(a.length+b.length) ; i++) {
-            c[i]=b[i-a.length];
-        }
-*/
-
-        //java에서 지원하는 arraycopy 메소드, arrayUtil.concat과 동일한 기능
-        //System.arraycopy(Object src, int srcPos, Object dest, int destPos, int length);
-        System.arraycopy(a, 0, c, 0, a.length);
-        System.arraycopy(b, 0, c, a.length,  b.length );
-        return c;
-    }
-    public static void print(int[] a){
-        System.out.print("[");
-        //배열 a 출력
-        for (int j : a) {
-            System.out.print(j + " ");
-        }
-        System.out.print("]");
+        return word+"은/는 저의 사전에 없습니다.";
     }
 }
 public class Empty {
     public static void main(String[] args) {
-        int[] array1={1,5,7,9};
-        int[] array2={3,6,-1,100,77};
-        int[] array3=ArrayUtil.concat(array1,array2);
-        ArrayUtil.print(array3);
+        Scanner input=new Scanner(System.in);
+        System.out.println("한글 단어 검색 프로그램입니다.");
+        while (true){
+            System.out.print("한글 단어?");
+            String word=input.next();
+            if(word.equals("그만"))break;
+            String result=Dictionary.kor2Eng(word);
+            System.out.println(result);
+        }
     }
 }
