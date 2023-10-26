330657fc752cf8c0f5eaa8cf259409e09eb1028b
Shin Jiyong <shinjiyong0904@gmail.com>
2023-10-26 14:06:24 +0900

    question 11

--- src/Empty.java
+++ src/Empty.java
@@ -1,25 +1,88 @@
 import java.util.Scanner;
 
-class Dictionary{
-    private static String [] kor={"사랑","아기","돈","미래","희망"};
-    private static String [] eng={"love","baby","money","future","hope"};
-    public static String kor2Eng(String word){
-        for (int i = 0; i < kor.length; i++) {
-            if(kor[i].equals(word))return word+"은/는 "+eng[i];
-        }
-        return word+"은/는 저의 사전에 없습니다.";
+class Add{
+    int a,b;
+    void setValue(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    int calculate(){
+        return a+b;
+    }
+}
+class Sub{
+    int a,b;
+    void setValue(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    int calculate(){
+        return a-b;
+    }
+}
+class Mul{
+    int a,b;
+    void setValue(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    int calculate(){
+        return a*b;
+    }
+}
+class Div{
+    int a,b;
+    void setValue(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    int calculate(){
+        return a/b;
     }
 }
 public class Empty {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.println("한글 단어 검색 프로그램입니다.");
-        while (true){
-            System.out.print("한글 단어?");
-            String word=input.next();
-            if(word.equals("그만"))break;
-            String result=Dictionary.kor2Eng(word);
-            System.out.println(result);
+        Add add = new Add();Sub sub = new Sub();
+        Mul mul = new Mul();Div div = new Div();
+        System.out.print("두 정수와 연산자를 입력하시오>>");
+        int a= input.nextInt();
+        int b= input.nextInt();
+        String c=input.next();
+        int result;
+        switch (c) {
+            case "+" -> {
+                add.setValue(a, b);
+                result = add.calculate();
+                System.out.println(result);
+            }
+            case "-" -> {
+                sub.setValue(a, b);
+                result = sub.calculate();
+                System.out.println(result);
+            }
+            case "*" -> {
+                mul.setValue(a, b);
+                result = mul.calculate();
+                System.out.println(result);
+            }
+            case "/" -> {
+                div.setValue(a, b);
+                result = div.calculate();
+                System.out.println(result);
+            }
         }
+        /* 같은 기능을 하는 코드
+        switch(c){
+            case "+":add.setValue(a,b);result=add.calculate();
+                System.out.println(result);break;
+            case "-":sub.setValue(a,b);result=sub.calculate();
+                System.out.println(result);break;
+            case "*":mul.setValue(a,b);result= mul.calculate();
+                System.out.println(result);break;
+            case "/":div.setValue(a,b);result=div.calculate();
+                System.out.println(result);break;
+        }*/
+
     }
 }
